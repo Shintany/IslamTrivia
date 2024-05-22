bcc9b52b2c805d3238761102dd3aa32276ac5573
Shintany <irchadtuankitchil@gmail.com>
2024-05-22 20:34:19 +0200

    Initial Commit

---
+++ IslamTrivia/IslamTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  IslamTriviaApp.swift
+//  IslamTrivia
+//
+//  Created by Irchad TUAN-KITCHIL on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct IslamTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
